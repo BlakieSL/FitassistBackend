27a049025df58b16ffebe24ae158aab64c7dc89b
BlakieSL <126717303+BlakieSL@users.noreply.github.com>
2024-06-27 22:34:01 +0200

    test

---
+++ src/test/java/com/example/simplefullstackproject/SimpleFullStackProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplefullstackproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleFullStackProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
