7070227f53d65656e0305d67e29b30bb6567840a
BlakieSL <126717303+BlakieSL@users.noreply.github.com>
2024-06-28 00:52:03 +0200

    database set up

---
+++ src/main/java/com/example/simplefullstackproject/Models/DailyCartActivity.java
@@ -0,0 +1,24 @@
+package com.example.simplefullstackproject.Models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Data;
+
+@Entity
+@Table(name = "daily_cart_activity")
+@Data
+public class DailyCartActivity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotNull
+    @Positive
+    @Column(nullable = false)
+    private int time;
+
+    @ManyToOne
+    @JoinColumn(name = "Daily_Activity_Id", nullable = false)
+    private DailyActivity dailyCartActivity;
+}
