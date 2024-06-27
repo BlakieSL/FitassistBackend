91b55798812e1ac69c1b2b86270c7ca1240bc034
BlakieSL <126717303+BlakieSL@users.noreply.github.com>
2024-06-27 22:58:45 +0200

    update

--- src/main/java/com/example/simplefullstackproject/Models/Role.java
+++ src/main/java/com/example/simplefullstackproject/Models/Role.java
@@ -1,4 +1,25 @@
 package com.example.simplefullstackproject.Models;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank
+    @Size(min = 4, max = 5)
+    @Column(nullable = false, length = 5)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users = new HashSet<>();
 }
