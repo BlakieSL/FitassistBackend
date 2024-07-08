09fd0960d7334602cd373b39590987c6e19e130f
BlakieSL <126717303+BlakieSL@users.noreply.github.com>
2024-07-08 21:53:13 +0200

    cart

--- src/main/java/com/example/simplefullstackproject/Dtos/FoodDtoResponse.java
+++ src/main/java/com/example/simplefullstackproject/Dtos/FoodDtoResponse.java
@@ -14,6 +14,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class FoodDtoResponse {
+    Integer id;
     @Size(max = 50)
     @NotBlank
     String name;
--- src/main/resources/static/cart.js
+++ src/main/resources/static/cart.js
@@ -17,3 +17,61 @@ export async function addToCart(){
     verifyResponse(response);
     alert('Food added successfully')
 }
+
+export async function fetchCartFood(){
+    const token = verifyToken();
+
+    const response = await fetch(`/api/cart/${getUserId()}`, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    });
+
+    verifyResponse(response);
+
+    const data = await response.json();
+    displayCartFood(data);
+}
+
+async function fetchCartActivity(){
+
+}
+
+function displayCartFood(foods) {
+    const foodList = document.getElementById('cartFoodList');
+    foodList.innerHTML = '';
+    if (foods.length > 0) {
+        foods.forEach(food => {
+            const foodLink = document.createElement('a');
+            foodLink.href = `/food/${food.id}`;
+            foodLink.className = 'food-link';
+
+            const foodDiv = document.createElement('div');
+            foodDiv.className = 'food-item';
+
+            const foodName = document.createElement('h2');
+            foodName.textContent = food.name;
+            foodDiv.appendChild(foodName);
+
+            const foodCalories = document.createElement('p');
+            foodCalories.textContent = `Calories: ${food.calories}`;
+            foodDiv.appendChild(foodCalories);
+
+            const amount = document.createElement('p');
+            amount.textContent =   `Amount: ${food.amount}`
+            foodDiv.appendChild(amount);
+
+            foodLink.appendChild(foodDiv);
+            foodList.appendChild(foodLink);
+        });
+    } else {
+        document.getElementById('cartFoodListError').textContent = 'There is no foods in cart';
+    }
+}
+function displayCartActivity(activities){
+
+}
+
+function displayInfo(){
+
+}
