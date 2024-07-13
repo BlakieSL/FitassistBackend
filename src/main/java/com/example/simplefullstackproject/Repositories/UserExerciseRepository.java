eb00cb86a7e59b4f92167cf16b5a65b57e8c1bce
Artem Ladnyi <artemladnyi@MacBook-Pro-Artem.local>
2024-07-13 12:54:46 +0200

    userExercise service

--- src/main/java/com/example/simplefullstackproject/Repositories/UserExerciseRepository.java
+++ src/main/java/com/example/simplefullstackproject/Repositories/UserExerciseRepository.java
@@ -4,7 +4,9 @@ import com.example.simplefullstackproject.Models.UserExercise;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserExerciseRepository extends JpaRepository<UserExercise, Integer> {
   List<UserExercise> findByUserId(Integer userId);
+  Optional<UserExercise> findByUserIdAndExerciseId(Integer userId, Integer exerciseId);
 }
---
+++ src/main/java/com/example/simplefullstackproject/Services/UserExerciseService.java
@@ -0,0 +1,57 @@
+package com.example.simplefullstackproject.Services;
+
+import com.example.simplefullstackproject.Models.Exercise;
+import com.example.simplefullstackproject.Models.User;
+import com.example.simplefullstackproject.Models.UserExercise;
+import com.example.simplefullstackproject.Repositories.ExerciseRepository;
+import com.example.simplefullstackproject.Repositories.UserExerciseRepository;
+import com.example.simplefullstackproject.Repositories.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class UserExerciseService {
+    private final ValidationHelper validationHelper;
+    private final UserExerciseRepository userExerciseRepository;
+    private final ExerciseRepository exerciseRepository;
+    private final UserRepository userRepository;
+
+    public UserExerciseService(
+            final ValidationHelper validationHelper,
+            final UserExerciseRepository userExerciseRepository,
+            final ExerciseRepository exerciseRepository,
+            final UserRepository userRepository){
+        this.validationHelper = validationHelper;
+        this.userExerciseRepository = userExerciseRepository;
+        this.exerciseRepository = exerciseRepository;
+        this.userRepository = userRepository;
+    }
+
+    @Transactional
+    public void addExerciseToUser(Integer exerciseId, Integer userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new NoSuchElementException(
+                        "User with id: " + userId + " not found"));
+
+        Exercise exercise = exerciseRepository.findById(exerciseId)
+                .orElseThrow(() -> new NoSuchElementException(
+                        "Exercise with id: " + exerciseId + " not found"));
+
+        UserExercise userExercise = new UserExercise();
+        userExercise.setUser(user);
+        userExercise.setExercise(exercise);
+        userExerciseRepository.save(userExercise);
+    }
+
+    @Transactional
+    public void deleteExerciseFromUser(Integer exerciseId, Integer userId) {
+        UserExercise userExercise = userExerciseRepository.findByUserIdAndExerciseId(userId, exerciseId)
+                .orElseThrow(() -> new NoSuchElementException(
+                        "UserExercise with user id: " + userId +
+                                " and exercise id: " + exerciseId + " not found"));
+
+        userExerciseRepository.delete(userExercise);
+    }
+}
