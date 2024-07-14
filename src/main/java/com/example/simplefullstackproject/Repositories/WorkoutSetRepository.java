b17ee69f8bb13aa65c4a4e585f2714e380a4ebd5
Artem Ladnyi <artemladnyi@MacBook-Pro-Artem.local>
2024-07-14 09:35:46 +0200

    workout set service

--- src/main/java/com/example/simplefullstackproject/Repositories/WorkoutSetRepository.java
+++ src/main/java/com/example/simplefullstackproject/Repositories/WorkoutSetRepository.java
@@ -3,5 +3,8 @@ package com.example.simplefullstackproject.Repositories;
 import com.example.simplefullstackproject.Models.WorkoutSet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WorkoutSetRepository extends JpaRepository<WorkoutSet, Integer> {
+    List<WorkoutSet> findByWorkoutTypeId(Integer  workoutTypeId);
 }
