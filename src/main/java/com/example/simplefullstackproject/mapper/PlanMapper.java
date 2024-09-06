9d817e8e15f3a8414c065a9b53a89e39f1153c45
BlakieSL <126717303+BlakieSL@users.noreply.github.com>
2024-09-06 20:57:22 +0200

    another categories

--- src/main/java/com/example/simplefullstackproject/mapper/PlanMapper.java
+++ src/main/java/com/example/simplefullstackproject/mapper/PlanMapper.java
@@ -1,14 +1,22 @@
 package com.example.simplefullstackproject.mapper;
 
+import com.example.simplefullstackproject.dto.PlanCategoryDto;
 import com.example.simplefullstackproject.dto.PlanDto;
 import com.example.simplefullstackproject.model.Plan;
+import com.example.simplefullstackproject.model.PlanCategory;
+import com.example.simplefullstackproject.repository.PlanCategoryRepository;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper(componentModel = "spring")
-public interface PlanMapper {
-    PlanDto toDto(Plan plan);
+public abstract class PlanMapper {
+    @Autowired
+    private PlanCategoryRepository planCategoryRepository;
+    public abstract PlanDto toDto(Plan plan);
 
     @Mapping(target = "id", ignore = true)
-    Plan toEntity(PlanDto dto);
+    public abstract Plan toEntity(PlanDto dto);
+
+    public abstract PlanCategoryDto toCategoryDto(PlanCategory planCategory);
 }
