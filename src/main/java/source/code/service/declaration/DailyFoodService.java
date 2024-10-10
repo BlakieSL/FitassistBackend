7a1d67679ac2f9e82341ebee8d579c075720a9b7
BlakieSL <126717303+BlakieSL@users.noreply.github.com>
2024-10-10 18:52:39 +0200

    some naming refactoring

--- src/main/java/source/code/service/declaration/DailyFoodService.java
+++ src/main/java/source/code/service/declaration/DailyFoodService.java
@@ -3,13 +3,13 @@ package source.code.service.declaration;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.github.fge.jsonpatch.JsonPatchException;
 import com.github.fge.jsonpatch.mergepatch.JsonMergePatch;
-import source.code.dto.request.DailyCartFoodCreateDto;
+import source.code.dto.request.DailyFoodItemCreateDto;
 import source.code.dto.response.DailyFoodsResponseDto;
 
 public interface DailyFoodService {
     void updateDailyCarts();
-    void addFoodToDailyCartFood(int userId, int foodId, DailyCartFoodCreateDto dto);
-    void removeFoodFromDailyCartFood(int userId, int foodId);
-    void updateDailyCartFood(int userId, int foodId, JsonMergePatch patch) throws JsonPatchException, JsonProcessingException;
-    DailyFoodsResponseDto getFoodsFromDailyCartFood(int userId);
+    void addFoodToDailyFoodItem(int userId, int foodId, DailyFoodItemCreateDto dto);
+    void removeFoodFromDailyFoodItem(int userId, int foodId);
+    void updateDailyFoodItem(int userId, int foodId, JsonMergePatch patch) throws JsonPatchException, JsonProcessingException;
+    DailyFoodsResponseDto getFoodsFromDailyFoodItem(int userId);
 }
