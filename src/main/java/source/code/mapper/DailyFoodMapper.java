6ffa7bde951359106b465d7081a1c71cfecfc06d
BlakieSL <126717303+BlakieSL@users.noreply.github.com>
2024-10-10 19:03:48 +0200

    DailyFoodMapper added for refactoring of DailyFoodServiceImpl

---
+++ src/main/java/source/code/mapper/DailyFoodMapper.java
@@ -0,0 +1,37 @@
+package source.code.mapper;
+
+import org.mapstruct.AfterMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import source.code.dto.response.FoodCalculatedMacrosResponseDto;
+import source.code.model.DailyFoodItem;
+import source.code.model.Food;
+
+@Mapper(componentModel = "spring")
+public abstract class DailyFoodMapper {
+    @Mapping(target = "id", source = "dailyFoodItem.food.id")
+    @Mapping(target = "name", source = "dailyFoodItem.food.name")
+    @Mapping(target = "calories", ignore = true)
+    @Mapping(target = "protein", ignore = true)
+    @Mapping(target = "fat", ignore = true)
+    @Mapping(target = "carbohydrates", ignore = true)
+    @Mapping(target = "categoryId", source = "dailyFoodItem.food.foodCategory.id")
+    @Mapping(target = "categoryName", source = "dailyFoodItem.food.foodCategory.name")
+    @Mapping(target = "amount", source = "dailyFoodItem.amount")
+    public abstract FoodCalculatedMacrosResponseDto toFoodCalculatedMacrosResponseDto(DailyFoodItem dailyFoodItem);
+
+    @AfterMapping
+    protected void setCalculatedMacros(
+            @MappingTarget FoodCalculatedMacrosResponseDto responseDto,
+            DailyFoodItem dailyFoodItem) {
+
+        Food food = dailyFoodItem.getFood();
+        double factor = (double) dailyFoodItem.getAmount() / 100;
+
+        responseDto.setCalories(food.getCalories() * factor);
+        responseDto.setProtein(food.getProtein() * factor);
+        responseDto.setFat(food.getFat() * factor);
+        responseDto.setCarbohydrates(food.getCarbohydrates() * factor);
+    }
+}
