df43b39586f07e2d395632fc5ff3ef86e725a751
BlakieSL <126717303+BlakieSL@users.noreply.github.com>
2024-10-12 00:48:16 +0200

    unit test template for FoodServiceTets

---
+++ src/test/java/source/code/service/implementation/FoodServiceTest.java
@@ -0,0 +1,127 @@
+package source.code.service.implementation;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import source.code.helper.ValidationHelper;
+import source.code.mapper.FoodMapper;
+import source.code.repository.FoodCategoryRepository;
+import source.code.repository.FoodRepository;
+import source.code.repository.UserFoodRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class FoodServiceTest {
+  @Mock
+  private FoodRepository foodRepository;
+  @Mock
+  private FoodMapper foodMapper;
+  @Mock
+  private ValidationHelper validationHelper;
+  @Mock
+  private FoodCategoryRepository foodCategoryRepository;
+  @Mock
+  private UserFoodRepository userFoodRepository;
+
+  @InjectMocks
+  private FoodServiceImpl foodService;
+  @BeforeEach
+  void setup() {
+
+  }
+
+  @Test
+  void createFood_shouldCreate_whenValidationPassed() {
+
+  }
+
+  @Test
+  void createFood_shouldThrowException_whenValidationFails() {
+
+  }
+
+  @Test
+  void getFood_shouldReturnFoodResponseDto_whenFoodFound() {
+
+  }
+
+  @Test
+  void getFood_shouldNotMap_whenFooNotFound() {
+
+  }
+
+  @Test
+  void getAllFoods_shouldReturnAllFoods_whenFoodsFound() {
+
+  }
+
+  @Test
+  void getAllFoods_shouldReturnEmptyList_whenFoodNotFound() {
+
+  }
+
+  @Test
+  void calculateFoodMacros_shouldCalculateFoodMacros_whenValidationPassedAndFoodFound() {
+
+  }
+
+  @Test
+  void calculateFoodMacros_shouldThrowException_whenValidationFails() {
+
+  }
+
+  @Test
+  void calculateFoodMacros_shouldThrowException_whenFoodNotFound() {
+
+  }
+
+  @Test
+  void searchFoods_shouldReturnFoods_whenFoodsFound() {
+
+  }
+
+  @Test
+  void searchFoods_shouldReturnEmptyList_whenNoFoodsFound() {
+
+  }
+
+  @Test
+  void getFoodsByUser_shouldReturnFoods_whenUserAndFoodsFound() {
+
+  }
+
+  @Test
+  void getFoodsByUser_shouldThrowException_whenUserNotFound() {
+
+  }
+
+  @Test
+  void getFoodsByUser_shouldReturnEmptyList_whenNoFoodsFound() {
+
+  }
+
+  @Test
+  void getAllCategories_shouldReturnFoodCategories_whenFoodCategoriesFound() {
+
+  }
+
+  @Test
+  void getAllCategories_shouldReturnEmptyList_whenNoFoodCategoriesFound() {
+
+  }
+
+  @Test
+  void getFoodsByCategory_shouldReturnFoods_whenFoodsFound() {
+
+  }
+
+  @Test
+  void getFoodsByCategory_shouldReturnEmptyList_whenNoFoodsFound() {
+
+  }
+}
+
+
+
