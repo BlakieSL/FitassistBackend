2745369e3092d139d3262a32f5421f9a3e1c8f37
BlakieSL <126717303+BlakieSL@users.noreply.github.com>
2024-10-12 20:35:32 +0200

    more templates

--- src/main/java/source/code/mapper/WorkoutMapper.java
+++ src/main/java/source/code/mapper/WorkoutMapper.java
@@ -1,9 +1,9 @@
 package source.code.mapper;
 
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import source.code.dto.WorkoutDto;
-import source.code.model.Workout.Workout;
+        import org.mapstruct.*;
+        import source.code.dto.WorkoutDto;
+        import source.code.model.Workout.Workout;
+        import source.code.model.Workout.WorkoutType;
 
 @Mapper(componentModel = "spring")
 public interface WorkoutMapper {
@@ -11,5 +11,10 @@ public interface WorkoutMapper {
   WorkoutDto toDto(Workout workout);
 
   @Mapping(target = "id", ignore = true)
-  Workout toEntity(WorkoutDto dto);
+  Workout toEntity(WorkoutDto dto, @Context WorkoutType workoutType);
+
+  @AfterMapping
+  default void mapWorkoutType(@MappingTarget Workout workout, @Context WorkoutType workoutType) {
+    workout.setWorkoutType(workoutType);
+  }
 }
--- src/main/java/source/code/repository/RecipeFoodRepository.java
+++ src/main/java/source/code/repository/RecipeFoodRepository.java
@@ -6,5 +6,6 @@ import source.code.model.Recipe.RecipeFood;
 import java.util.Optional;
 
 public interface RecipeFoodRepository extends JpaRepository<RecipeFood, Long> {
+  boolean existsByRecipeIdAndFoodId(int recipeId, int foodId);
   Optional<RecipeFood> findByRecipeIdAndFoodId(int recipeId, int foodId);
 }
