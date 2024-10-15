fe2ece3489e11a4ca008099304edcce28dd5ba4b
Artem Ladnyi <ladnyiartem@gmail.com>
2024-10-15 13:58:09 +0200

    update

--- src/main/java/source/code/FitassistApplication.java
+++ src/main/java/source/code/FitassistApplication.java
@@ -1,11 +1,14 @@
 package source.code;
 
+import com.cosium.spring.data.jpa.entity.graph.repository.support.EntityGraphJpaRepositoryFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableJpaRepositories(repositoryFactoryBeanClass = EntityGraphJpaRepositoryFactoryBean.class)
 public class FitassistApplication {
 
   public static void main(String[] args) {
