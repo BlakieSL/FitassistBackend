42ecbbdf36a8629f9546a9acefd081772792902f
BlakieSL <126717303+BlakieSL@users.noreply.github.com>
2024-10-17 08:59:24 +0200

    cache clear for food service, new udpate and delete methods

--- src/main/java/source/code/service/declaration/FoodService.java
+++ src/main/java/source/code/service/declaration/FoodService.java
@@ -1,7 +1,11 @@
 package source.code.service.declaration;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.github.fge.jsonpatch.JsonPatchException;
+import com.github.fge.jsonpatch.mergepatch.JsonMergePatch;
 import source.code.dto.request.CalculateFoodMacrosRequestDto;
 import source.code.dto.request.FoodCreateDto;
+import source.code.dto.request.FoodUpdateDto;
 import source.code.dto.request.SearchRequestDto;
 import source.code.dto.response.FoodCalculatedMacrosResponseDto;
 import source.code.dto.response.FoodCategoryResponseDto;
@@ -12,11 +16,15 @@ import java.util.List;
 public interface FoodService {
   FoodResponseDto createFood(FoodCreateDto request);
 
-  FoodResponseDto getFood(int id);
+  void updateFood(int foodId, JsonMergePatch patch) throws JsonPatchException, JsonProcessingException;
+
+  void deleteFood(int foodId);
+
+  FoodResponseDto getFood(int foodId);
 
   List<FoodResponseDto> getAllFoods();
 
-  FoodCalculatedMacrosResponseDto calculateFoodMacros(int id, CalculateFoodMacrosRequestDto request);
+  FoodCalculatedMacrosResponseDto calculateFoodMacros(int foodId, CalculateFoodMacrosRequestDto request);
 
   List<FoodResponseDto> searchFoods(SearchRequestDto request);
 
