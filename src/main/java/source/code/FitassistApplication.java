83c8b508e5e5cfab7e92e9aae96948699e0a3ab4
Artem Ladnyi <ladnyiartem@gmail.com>
2024-10-30 10:46:36 +0100

    fixed bugs with serialization

--- src/main/java/source/code/FitassistApplication.java
+++ src/main/java/source/code/FitassistApplication.java
@@ -3,6 +3,7 @@ package source.code;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
