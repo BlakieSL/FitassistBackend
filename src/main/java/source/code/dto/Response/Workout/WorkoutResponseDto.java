32bf7bfb4336a40f1eb6b31f797ebea69fb8797a
BlakieSL <126717303+BlakieSL@users.noreply.github.com>
2024-11-02 16:46:28 +0100

    skeleton for workoutservice

---
+++ src/main/java/source/code/dto/Response/Workout/WorkoutResponseDto.java
@@ -0,0 +1,18 @@
+package source.code.dto.Response.Workout;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class WorkoutResponseDto {
+  private int id;
+  private String name;
+  private int time;
+}
