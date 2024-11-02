ee067827c6b0509df1384b166ed791392b04553f
BlakieSL <126717303+BlakieSL@users.noreply.github.com>
2024-11-02 11:11:46 +0100

    added all specifications for filtering by likes and saves

--- src/main/java/source/code/specification/specification/RecipeSpecification.java
+++ src/main/java/source/code/specification/specification/RecipeSpecification.java
@@ -5,7 +5,10 @@ import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
 import org.springframework.lang.NonNull;
+import source.code.helper.Enum.Model.LikesAndSaves;
 import source.code.helper.Enum.Model.RecipeField;
+import source.code.helper.TriFunction;
+import source.code.model.Exercise.Exercise;
 import source.code.model.Recipe.Recipe;
 import source.code.model.Recipe.RecipeCategoryAssociation;
 import source.code.pojo.FilterCriteria;
@@ -15,14 +18,23 @@ import java.util.Optional;
 import java.util.function.BiFunction;
 
 public class RecipeSpecification extends BaseSpecification<Recipe>{
-  private final Map<String, BiFunction<Root<Recipe>, CriteriaBuilder, Predicate>> fieldHandlers;
+  private final Map<String, TriFunction<Root<Recipe>,
+          CriteriaQuery<?>, CriteriaBuilder, Predicate>> fieldHandlers;
 
   public RecipeSpecification(@NonNull FilterCriteria criteria) {
     super(criteria);
     fieldHandlers = Map.of(
             RecipeField.CATEGORY.name(),
-            (root, builder) -> handleManyToManyProperty(root, RecipeField.CATEGORY.getFieldName(),
-                    RecipeCategoryAssociation.RECIPE_CATEGORY, builder)
+            (root, quer, builder) -> handleManyToManyProperty(root, RecipeField.CATEGORY.getFieldName(),
+                    RecipeCategoryAssociation.RECIPE_CATEGORY, builder),
+
+            LikesAndSaves.LIKES.name(),
+            (root, query, builder) ->
+                    handleLikesProperty(root, LikesAndSaves.USER_EXERCISES.getFieldName(), query, builder),
+
+            LikesAndSaves.SAVES.name(),
+            (root, query, builder) ->
+                    handleLikesProperty(root, LikesAndSaves.USER_EXERCISES.getFieldName(), query, builder)
     );
   }
 
@@ -30,7 +42,7 @@ public class RecipeSpecification extends BaseSpecification<Recipe>{
   public Predicate toPredicate(@NonNull Root<Recipe> root, @NonNull CriteriaQuery<?> query,
                                @NonNull CriteriaBuilder builder) {
     return Optional.ofNullable(fieldHandlers.get(criteria.getFilterKey()))
-            .map(handler -> handler.apply(root, builder))
+            .map(handler -> handler.apply(root, query, builder))
             .orElseThrow(() -> new IllegalStateException("Unexpected filter key: " + criteria.getFilterKey()));
   }
 }
