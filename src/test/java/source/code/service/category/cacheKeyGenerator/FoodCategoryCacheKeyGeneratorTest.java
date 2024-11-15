01ff2e5470f6e292ca02224ba30508a1a5b5b557
BlakieSL <126717303+BlakieSL@users.noreply.github.com>
2024-11-15 10:35:18 +0100

    tests

---
+++ src/test/java/source/code/service/category/cacheKeyGenerator/FoodCategoryCacheKeyGeneratorTest.java
@@ -0,0 +1,24 @@
+package source.code.service.category.cacheKeyGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import source.code.service.implementation.category.cacheKeyGenerator.FoodCategoryCacheKeyGeneratorImpl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class FoodCategoryCacheKeyGeneratorTest {
+    @InjectMocks
+    private FoodCategoryCacheKeyGeneratorImpl foodCategoryCacheKeyGenerator;
+
+    @Test
+    void generateCacheKey_shouldReturn() {
+        String expectedCacheKey = "FOOD_CATEGORIES";
+
+        String actualCacheKey = foodCategoryCacheKeyGenerator.generateCacheKey();
+
+        assertEquals(expectedCacheKey, actualCacheKey);
+    }
+}
