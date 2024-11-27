66b2c110fd3e25df08758d7ba50c2b56efd7521f
Artem Ladnyi <ladnyiartem@gmail.com>
2024-11-27 11:21:30 +0100

    tests

--- src/main/java/source/code/config/RedissonRateLimitConfig.java
+++ src/main/java/source/code/config/RedissonRateLimitConfig.java
@@ -1,13 +1,24 @@
 package source.code.config;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConfigurationProperties(prefix = "rate-limiter")
+@Getter
+@Setter
 public class RedissonRateLimitConfig {
+    private int userRate;
+    private int userInterval;
+    private int keyRate;
+    private int keyInterval;
+
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
