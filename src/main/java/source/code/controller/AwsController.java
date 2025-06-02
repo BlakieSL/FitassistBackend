f82a1164323e510195b4b5365f1cc4719dedf9bb
Artem <ladnyiartem@gmail.com>
2025-06-02 13:19:32 +0200

    added aws s3 integration, fixed security filters

---
+++ src/main/java/source/code/controller/AwsController.java
@@ -0,0 +1,20 @@
+package source.code.controller;
+
+import org.springframework.web.bind.annotation.*;
+import source.code.service.declaration.aws.AwsS3Service;
+
+@RestController
+@RequestMapping("/api/s3")
+public class AwsController {
+    private final AwsS3Service awsS3Service;
+
+    public AwsController(AwsS3Service awsS3Service) {
+        this.awsS3Service = awsS3Service;
+    }
+
+    @GetMapping("/{imageName}")
+    public String getImageUrl(@PathVariable String imageName) {
+        System.out.println("Fetching image URL for: " + imageName);
+        return awsS3Service.getImage(imageName);
+    }
+}
