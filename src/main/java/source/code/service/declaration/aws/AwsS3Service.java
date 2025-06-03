97a95349fbe362ca706965b94b0f92281609904a
Artem <ladnyiartem@gmail.com>
2025-06-03 11:25:02 +0200

    added removeMedia from aws s3 method, fixed RedissonRateLimitConfig to accept host and port from .yaml, added tests for new method

--- src/main/java/source/code/service/declaration/aws/AwsS3Service.java
+++ src/main/java/source/code/service/declaration/aws/AwsS3Service.java
@@ -3,4 +3,5 @@ package source.code.service.declaration.aws;
 public interface AwsS3Service {
     String uploadImage(byte[] imageBytes);
     String getImage(String imageName);
+    void deleteImage(String imageName);
 }
