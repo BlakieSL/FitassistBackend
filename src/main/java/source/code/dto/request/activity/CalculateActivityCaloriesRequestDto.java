fbbcfd3d08bca9f4c7381f252e2278ae057d0e92
Artem <ladnyiartem@gmail.com>
2025-07-04 23:38:47 +0200

    added validation logic for user health-related information

--- src/main/java/source/code/dto/request/activity/CalculateActivityCaloriesRequestDto.java
+++ src/main/java/source/code/dto/request/activity/CalculateActivityCaloriesRequestDto.java
@@ -7,6 +7,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -15,4 +17,6 @@ public class CalculateActivityCaloriesRequestDto {
     @NotNull
     @Positive
     private int time = 1;
+
+    private BigDecimal weight;
 }
