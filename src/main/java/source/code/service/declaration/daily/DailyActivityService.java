8f15523642c42dbb8ffea84660e0da55abb695ce
Artem Ladnyi <ladnyiartem@gmail.com>
2025-07-10 16:50:26 +0200

    Added logic into DailyActivityController to allow write requests to only owners, added service logic to get/add/update/remove on different dates + test coverage, Added custom test annotation which includes common annotations for integration tests

--- src/main/java/source/code/service/declaration/daily/DailyActivityService.java
+++ src/main/java/source/code/service/declaration/daily/DailyActivityService.java
@@ -3,15 +3,16 @@ package source.code.service.declaration.daily;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.github.fge.jsonpatch.JsonPatchException;
 import com.github.fge.jsonpatch.mergepatch.JsonMergePatch;
+import source.code.dto.request.activity.DailyActivitiesGetDto;
 import source.code.dto.request.activity.DailyActivityItemCreateDto;
 import source.code.dto.response.daily.DailyActivitiesResponseDto;
 
 public interface DailyActivityService {
     void addActivityToDailyCart(int activityId, DailyActivityItemCreateDto dto);
 
-    void removeActivityFromDailyCart(int activityId);
+    void removeActivityFromDailyCart(int dailyActivityItemId);
 
-    void updateDailyActivityItem(int activityId, JsonMergePatch patch) throws JsonPatchException, JsonProcessingException;
+    void updateDailyActivityItem(int dailyActivityItemId, JsonMergePatch patch) throws JsonPatchException, JsonProcessingException;
 
-    DailyActivitiesResponseDto getActivitiesFromDailyCart();
+    DailyActivitiesResponseDto getActivitiesFromDailyCart(DailyActivitiesGetDto request);
 }
