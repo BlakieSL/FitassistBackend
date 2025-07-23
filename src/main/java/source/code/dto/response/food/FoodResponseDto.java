4163eef772222218f99d178422f210a773eef44c
Artem <ladnyiartem@gmail.com>
2025-07-23 13:07:08 +0200

    added search, text integration tests

--- src/main/java/source/code/dto/response/food/FoodResponseDto.java
+++ src/main/java/source/code/dto/response/food/FoodResponseDto.java
@@ -1,9 +1,6 @@
 package source.code.dto.response.food;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import source.code.helper.BaseUserEntity;
 
 import java.math.BigDecimal;
@@ -12,8 +9,10 @@ import java.math.BigDecimal;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString(onlyExplicitlyIncluded = true)
 public class FoodResponseDto implements BaseUserEntity {
     private Integer id;
+    @ToString.Include
     private String name;
     private BigDecimal calories;
     private BigDecimal protein;
--- src/main/java/source/code/service/declaration/search/LuceneInitialLoadService.java
+++ src/main/java/source/code/service/declaration/search/LuceneInitialLoadService.java
@@ -2,4 +2,5 @@ package source.code.service.declaration.search;
 
 public interface LuceneInitialLoadService {
     void indexAll();
+    public void clearIndexDirectory();
 }
