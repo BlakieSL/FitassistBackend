238fca24399473069122b7659faab80f3ebed07b
Artem <ladnyiartem@gmail.com>
2025-08-16 18:19:52 +0200

    added aws SES integration, added dto for creating email and service

---
+++ src/main/java/source/code/service/implementation/email/EmailServiceImpl.java
@@ -0,0 +1,37 @@
+package source.code.service.implementation.email;
+
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.ses.SesClient;
+import software.amazon.awssdk.services.ses.model.*;
+import source.code.dto.request.email.EmailRequestDto;
+import source.code.service.declaration.email.EmailService;
+
+@Service
+public class EmailServiceImpl implements EmailService {
+    private final SesClient sesClient;
+
+    public EmailServiceImpl(SesClient sesClient) {
+        this.sesClient = sesClient;
+    }
+
+    @Override
+    public void sendEmail(EmailRequestDto emailRequest) {
+        SendEmailRequest request = SendEmailRequest.builder()
+                .source(emailRequest.getFromEmail())
+                .destination(Destination.builder()
+                        .toAddresses(emailRequest.getToEmails())
+                        .build())
+                .message(Message.builder()
+                        .subject(Content.builder().data(emailRequest.getSubject()).build())
+                        .body(Body.builder()
+                                .html(emailRequest.isHtml() ?
+                                        Content.builder().data(emailRequest.getContent()).build() : null)
+                                .text(!emailRequest.isHtml() ?
+                                        Content.builder().data(emailRequest.getContent()).build() : null)
+                                .build())
+                        .build())
+                .build();
+
+        sesClient.sendEmail(request);
+    }
+}
