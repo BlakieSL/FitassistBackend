03fd569883fd12dc43333568d0fdde3bdb41ce31
Artem <ladnyiartem@gmail.com>
2025-11-23 13:21:21 +0100

    Added L1 cache as Caffeine, setted up composite cache, created controller to monitor Caffeine stats

--- src/main/java/source/code/config/RedisCachingConfig.java
+++ src/main/java/source/code/config/RedisCachingConfig.java
@@ -1,12 +1,16 @@
 package source.code.config;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.cache.support.CompositeCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -15,18 +19,27 @@ import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 
 @EnableAutoConfiguration(exclude = RedisRepositoriesAutoConfiguration.class)
 @Configuration
 @EnableCaching
 public class RedisCachingConfig {
+
     @ConditionalOnProperty(name = "redis-flag.enabled", havingValue = "true")
     @Bean
+    @Primary
     public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        CaffeineCacheManager caffeineManager = new CaffeineCacheManager();
+        caffeineManager.setCaffeine(Caffeine.newBuilder()
+                .maximumSize(5000)
+                .expireAfterWrite(2, TimeUnit.MINUTES)
+                .recordStats());
+
         JdkSerializationRedisSerializer jdkSerializer = new JdkSerializationRedisSerializer();
 
-        RedisCacheConfiguration defaultCacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+        RedisCacheConfiguration redisCacheConfig = RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofMinutes(60))
                 .serializeKeysWith(RedisSerializationContext
                         .SerializationPair
@@ -35,8 +48,10 @@ public class RedisCachingConfig {
                         .SerializationPair
                         .fromSerializer(jdkSerializer));
 
-        return RedisCacheManager.builder(redisConnectionFactory)
-                .cacheDefaults(defaultCacheConfig)
+        RedisCacheManager redisManager = RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(redisCacheConfig)
                 .build();
+
+        return new CompositeCacheManager(caffeineManager, redisManager);
     }
 }
