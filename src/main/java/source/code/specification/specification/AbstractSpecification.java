281857c26c527b820316edb90ce9da3699098347
Artem <ladnyiartem@gmail.com>
2025-12-08 23:02:29 +0100

    refactored specification setup to add additional abstraction layer to remove duplication.

---
+++ src/main/java/source/code/specification/specification/AbstractSpecification.java
@@ -0,0 +1,33 @@
+package source.code.specification.specification;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.lang.NonNull;
+import source.code.dto.pojo.FilterCriteria;
+import source.code.service.implementation.specificationHelpers.SpecificationDependencies;
+import source.code.specification.PredicateContext;
+
+public abstract class AbstractSpecification<T, F extends Enum<F>> implements Specification<T> {
+
+    protected final FilterCriteria criteria;
+    protected final SpecificationDependencies dependencies;
+
+    protected AbstractSpecification(FilterCriteria criteria, SpecificationDependencies dependencies) {
+        this.criteria = criteria;
+        this.dependencies = dependencies;
+    }
+
+    @Override
+    public Predicate toPredicate(@NonNull Root<T> root, CriteriaQuery<?> query, @NonNull CriteriaBuilder builder) {
+        F field = dependencies.getFieldResolver().resolveField(criteria, getFieldClass());
+        PredicateContext<T> context = new PredicateContext<>(builder, root, query, criteria);
+        return buildPredicateForField(context, field);
+    }
+
+    protected abstract Class<F> getFieldClass();
+
+    protected abstract Predicate buildPredicateForField(PredicateContext<T> context, F field);
+}
