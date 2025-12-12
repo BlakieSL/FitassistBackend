aa037ac2b8b523e231cdae36be04c46af86979cd
Artem <ladnyiartem@gmail.com>
2025-12-12 18:23:25 +0100

    enhance ForumThreadResponseDto: add author details and interaction counts; update related services and tests

--- src/main/java/source/code/dto/pojo/projection/recipe/RecipeInteractionDateProjection.java
+++
@@ -1,8 +0,0 @@
-package source.code.dto.pojo.projection.recipe;
-
-import java.time.LocalDateTime;
-
-public interface RecipeInteractionDateProjection {
-    Integer getRecipeId();
-    LocalDateTime getCreatedAt();
-}
--- src/main/java/source/code/service/declaration/thread/ForumThreadPopulationService.java
+++ src/main/java/source/code/service/declaration/thread/ForumThreadPopulationService.java
@@ -1,9 +1,11 @@
 package source.code.service.declaration.thread;
 
+import source.code.dto.response.forumThread.ForumThreadResponseDto;
 import source.code.dto.response.forumThread.ForumThreadSummaryDto;
 
 import java.util.List;
 
 public interface ForumThreadPopulationService {
     void populate(List<ForumThreadSummaryDto> threads);
+    void populate(ForumThreadResponseDto thread);
 }
