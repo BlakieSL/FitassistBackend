eb47b786235035ad69953c2393875a6a1dfcf98f
Artem <ladnyiartem@gmail.com>
2025-12-28 11:27:57 +0100

    create generic dto for other than food and activity searches

---
+++ src/main/java/source/code/dto/response/search/GenericSearchResponseDto.java
@@ -0,0 +1,16 @@
+package source.code.dto.response.search;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class GenericSearchResponseDto extends SearchResponseDto {
+
+	public GenericSearchResponseDto(Integer id, String name, String type) {
+		super(id, name, type);
+	}
+
+}
