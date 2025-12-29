4087350b5851789999c7e110e4be94cca5b75a50
Artem <ladnyiartem@gmail.com>
2025-12-29 16:47:38 +0100

    format

--- src/main/java/source/code/specification/SpecificationBuilder.java
+++ src/main/java/source/code/specification/SpecificationBuilder.java
@@ -31,7 +31,7 @@ public class SpecificationBuilder<T> {
 	public Specification<T> build() {
 		List<FilterCriteria> criteriaList = filterDto.getFilterCriteria();
 		if (criteriaList == null || criteriaList.isEmpty()) {
- 			return specificationFactory.createSpecification(new FilterCriteria(), dependencies);
+			return specificationFactory.createSpecification(new FilterCriteria(), dependencies);
 		}
 
 		Specification<T> result = specificationFactory.createSpecification(criteriaList.get(0), dependencies);
