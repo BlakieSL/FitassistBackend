e3299ee9190915dfb3d1da91852d11a9eb974925
Artem <ladnyiartem@gmail.com>
2025-12-30 23:55:04 +0100

    refactor: remove common cache clearing from listeners; update Plan handling to check visibility before adding to Lucene

--- src/main/java/source/code/dto/request/plan/PlanCreateDto.java
+++ src/main/java/source/code/dto/request/plan/PlanCreateDto.java
@@ -1,5 +1,6 @@
 package source.code.dto.request.plan;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -38,6 +39,7 @@ public class PlanCreateDto {
 
 	private List<Integer> categoryIds;
 
+	@Valid
 	private List<PlanInstructionCreateDto> instructions;
 
 }
--- src/main/java/source/code/helper/Enum/cache/CacheNames.java
+++ src/main/java/source/code/helper/Enum/cache/CacheNames.java
@@ -2,27 +2,17 @@ package source.code.helper.Enum.cache;
 
 public class CacheNames {
 
-	public static final String ALL_ACTIVITIES = "allActivities";
-
 	public static final String ACTIVITIES = "activities";
 
-	public static final String ALL_FOODS = "allFoods";
-
 	public static final String FOODS = "foods";
-
-	public static final String ALL_EXERCISES = "allExercises";
 
 	public static final String EXERCISES = "exercises";
 
 	public static final String EXERCISE_CATEGORIES = "exerciseCategories";
 
-	public static final String ALL_PLANS = "allPlans";
-
 	public static final String PLANS = "plans";
 
 	public static final String PLAN_CATEGORIES = "planCategories";
-
-	public static final String ALL_RECIPES = "allRecipes";
 
 	public static final String RECIPES = "recipes";
 
