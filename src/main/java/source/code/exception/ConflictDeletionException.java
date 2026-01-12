970991cdae5c84337baeba0441fab8017a27f1ba
Artem <ladnyiartem@gmail.com>
2026-01-12 23:21:43 +0100

    refactor: update global exception handling to return structured error responses

--- src/main/java/source/code/exception/ConflictDeletionException.java
+++
@@ -1,9 +0,0 @@
-package source.code.exception;
-
-public class ConflictDeletionException extends LocalizedException {
-
-	public <T> ConflictDeletionException(Class<T> entityClass, int id) {
-		super("ConflictDeletionException.message", null, entityClass.getSimpleName(), id);
-	}
-
-}
