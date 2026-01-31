e4df1a488e7b2336954612262d8fcb1cbcf0a609
Artem <ladnyiartem@gmail.com>
2026-01-31 20:57:32 +0100

    fixed calories burned calculation formula, refactored CalculationsService to follow SOLID and overall best practises

--- src/main/java/com/fitassist/backend/model/user/Gender.java
+++ src/main/java/com/fitassist/backend/model/user/Gender.java
@@ -1,7 +1,21 @@
 package com.fitassist.backend.model.user;
 
+import java.math.BigDecimal;
+
 public enum Gender {
 
-	MALE, FEMALE
+	MALE, FEMALE;
+
+	public BigDecimal calculateBMR(BigDecimal weight, BigDecimal height, int age) {
+		BigDecimal base = BigDecimal.valueOf(10)
+			.multiply(weight)
+			.add(BigDecimal.valueOf(6.25).multiply(height))
+			.subtract(BigDecimal.valueOf(5).multiply(BigDecimal.valueOf(age)));
+
+		return switch (this) {
+			case MALE -> base.add(BigDecimal.valueOf(5));
+			case FEMALE -> base.add(BigDecimal.valueOf(-161));
+		};
+	}
 
 }
