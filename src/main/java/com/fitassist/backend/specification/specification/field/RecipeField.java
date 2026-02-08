a10199f2d6479715134f189219db121a8e7c110c
Artem <ladnyiartem@gmail.com>
2026-02-08 01:16:23 +0100

    refactor: remove unused `SortingServiceImpl` implementation, redundant tests, and dependencies across multiple services and repositories. Optimize class structure and method usage.

--- src/main/java/com/fitassist/backend/specification/specification/field/RecipeField.java
+++ src/main/java/com/fitassist/backend/specification/specification/field/RecipeField.java
@@ -2,20 +2,6 @@ package com.fitassist.backend.specification.specification.field;
 
 public enum RecipeField {
 
-	SAVE, SAVED_BY_USER, LIKED_BY_USER, DISLIKED_BY_USER, CREATED_BY_USER, LIKE, CATEGORY("recipeCategoryAssociations"),
-	FOODS("recipeFoods");
-
-	private String fieldName;
-
-	RecipeField(String fieldName) {
-		this.fieldName = fieldName;
-	}
-
-	RecipeField() {
-	}
-
-	public String getFieldName() {
-		return fieldName;
-	}
+	SAVE, SAVED_BY_USER, LIKED_BY_USER, DISLIKED_BY_USER, CREATED_BY_USER, LIKE, CATEGORY, FOODS
 
 }
