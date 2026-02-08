e56690ee1a9798f59ddc6decf3dfaa323689b5f6
Artem <ladnyiartem@gmail.com>
2026-02-08 09:59:56 +0100

    fixed failing tests

--- src/main/java/com/fitassist/backend/specification/specification/filter/FilterOperation.java
+++ src/main/java/com/fitassist/backend/specification/specification/filter/FilterOperation.java
@@ -2,6 +2,6 @@ package com.fitassist.backend.specification.specification.filter;
 
 public enum FilterOperation {
 
-	EQUAL, NOT_EQUAL, GREATER_THAN, GREATER_THAN_EQUAL, LESS_THAN, LESS_THAN_EQUAL
+	EQUAL, NOT_EQUAL, GREATER_THAN, GREATER_THAN_EQUAL, LESS_THAN, LESS_THAN_EQUAL, CONTAINS
 
 }
